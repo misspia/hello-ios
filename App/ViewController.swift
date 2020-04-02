2d41ee30490e4dbe44d7382485c2cfe477c14cbd
misspia <pia.leung@mediamonks.com>
2020-04-02 23:48:42 +0200

    Attach increment/decrement methods to ui buttons

--- App/ViewController.swift
+++ App/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count: Int = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func increment(_ sender: UIButton) {
+        setLabel(sender);
+        count += 1
+        updateCountLabel()
+        
+    }
+    
+    @IBAction func decrement(_ sender: UIButton) {
+        setLabel(sender);
+        count -= 1
+        updateCountLabel()
+    }
+    
+    func setLabel(_ sender: UIButton) {
+        if let buttonLabel = sender.titleLabel?.text {
+            label.text = "clicked: \(buttonLabel)";
+        }
+    }
+    
+    func updateCountLabel() {
+        countLabel.text = String(count)
+    }
 }
 
